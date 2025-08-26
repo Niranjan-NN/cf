516289d55f600c2fc9149e5995f6ab4ca6affa92
akash0751 <akashsofficial05@gmail.com>
2025-08-26 12:24:02 +0530

    Added

--- vyapar/src/utils/axiosInstance.js
+++ vyapar/src/utils/axiosInstance.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = import.meta.env.VITE_API_URL;
 
@@ -7,9 +7,21 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
+// ✅ Attach token before every request
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 axiosInstance.interceptors.response.use(
-  res => res,
-  async err => {
+  (res) => res,
+  async (err) => {
     const originalRequest = err.config;
 
     if (
@@ -17,17 +29,22 @@ axiosInstance.interceptors.response.use(
       !originalRequest._retry
     ) {
       originalRequest._retry = true;
+
       try {
-        const response = await axios.post(`${api}/api/refresh-token`, {}, {
-          withCredentials: true,
-        });
+        // try refresh
+        const response = await axios.post(
+          `${api}/api/refresh-token`,
+          {},
+          { withCredentials: true }
+        );
 
         const newToken = response.data.token;
         localStorage.setItem("token", newToken);
 
+        // retry with new token
         originalRequest.headers.Authorization = `Bearer ${newToken}`;
         return axios(originalRequest);
-      } catch(refreshErr){
+      } catch (refreshErr) {
         localStorage.removeItem("token");
         window.location.href = "/login";
       }
